2c8d81ddffeb8dd3dc23b09db31e195453729315
Faudiac <115066002+Faudiac@users.noreply.github.com>
2023-04-15 17:26:43 +0500

    yup

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-// Set the date we're counting down to
-var countDownDate = new Date("January 1, 2024 00:00:25").getTime();
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-  // Get today's date and time
-  var now = new Date().getTime();
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
 
-  // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is finished, write some text  
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-}, 1000);
+}

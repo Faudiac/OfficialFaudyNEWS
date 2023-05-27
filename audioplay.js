962a090483b7a8e0569298d2c528f061e41bec03
Faudiac <115066002+Faudiac@users.noreply.github.com>
2023-05-27 23:26:52 +0500

    yes

---
+++ audioplay.js
@@ -0,0 +1,7 @@
+function openSearch() {
+    document.getElementById("AudioPlayer").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("AudioPlayer").style.display = "none";
+}
